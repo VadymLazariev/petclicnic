ed7d6363687474be8676cedf6043db5a2dc3577f
Vadym Lazariev <vadymlazariev@gmail.com>
2020-09-10 19:27:23 +0300

    adding POJOs.

---
+++ src/main/java/guru/springframework/sfgpetclinic/model/Person.java
@@ -0,0 +1,22 @@
+package guru.springframework.sfgpetclinic.model;
+
+public class Person {
+    private String firstname;
+    private String lastname;
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+}
